e2294f90f00b2ec9a9e5eefd59d4afa89120b4a5
Marcin Bejger <marcin.bejger@gmail.com>
2022-08-05 16:06:01 +0200

    login view started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Login } from "./views/Login/Login";
+
+
 function App() {
   return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+
+
+
     <div className="App">
       <header className="App-header">
         Cithara App
       </header>
     </div>
+    </>
   );
 }
 
